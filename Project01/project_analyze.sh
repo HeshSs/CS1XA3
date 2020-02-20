6297c8dd7e50b977966dad97611c13ed89583a76
salehh6 <salehh6@mcmaster.ca>
2020-02-20 20:17:12 +0000

    Made it possible to run features and custom_features only.

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [ "$1" != "feature" ] || [ "$2" -lt 2 ] 2> /dev/null || [ "$2" -gt 10 ] 2> /dev/null || [[ $2 != ?(-)+([0-9]) ]] ; then
+if [ "$1" != "feature" ] && [ "$1" != "custom_feature" ] || [ "$2" -lt 1 ] 2> /dev/null || [ "$2" -gt 7 ] 2> /dev/null || [[ $2 != ?(-)+([0-9]) ]] ; then
     echo "Usage:"
-    echo "  ./Project01/project_analyze feature feature#"
-elif [ "$2" == 2 ] ; then
+    echo "  ./Project01/project_analyze feature/custom_feature feature#"
+elif [ "$1" == "feature" ] && [ "$2" == 1 ] ; then
+    echo "Usage:"
+    echo "  ./Project01/project_analyze feature/custom_feature feature#"
+elif [ "$1" == "feature" ] && [ "$2" == 2 ] ; then
     if [[ -e ~/private/CS1XA3/Project01/fixme.log ]] ; then
         echo -n > ~/private/CS1XA3/Project01/fixme.log
     fi
@@ -14,7 +17,7 @@ elif [ "$2" == 2 ] ; then
           fi
     done
     echo "File fixme.log was successfully created in Project01 directory."
-elif [ "$2" == 3 ] ; then
+elif [ "$1" == "feature" ] && [ "$2" == 3 ] ; then
     first=`git log --oneline --graph | grep -i "merge" | head -1`
     if [ "${first:0:1}" = "*" ] && [ "${first:2:1}" = "|" ] ; then
         commit="${first:4:7}"
@@ -28,20 +31,18 @@ elif [ "$2" == 3 ] ; then
     if [[ -n "$commit" ]] ; then 
         git checkout "$commit"
     fi
-elif [ "$2" == 4 ] ; then 
+elif [ "$1" == "feature" ] && [ "$2" == 4 ] ; then 
     ls ~/private/CS1XA3 -aRlh | grep '^-' | sort -k 5 -rh | awk 'BEGIN {  print "Size Filename" 
                                                                           print "---- --------" } 
                                                                        {  print $5, $9 }'
-elif [ "$2" == 5 ] ; then
+elif [ "$1" == "feature" ] && [ "$2" == 5 ] ; then
     echo "Feature 5"
-elif [ "$2" == 6 ] ; then
+elif [ "$1" == "feature" ] && [ "$2" == 6 ] ; then
     echo "Feature 6"
-elif [ "$2" == 3 ] ; then
+elif [ "$1" == "feature" ] && [ "$2" == 7 ] ; then
     echo "Feature 7"
-elif [ "$2" == 8 ] ; then
-    echo "Feature 8"
-elif [ "$2" == 9 ] ; then
-    echo "Feature 9"
-elif [ "$2" == 10 ] ; then
-    echo "Feature 10"
+elif [ "$1" == "custom_feature" ] && [ "$2" == 1 ] ; then
+    echo "Custom Feature 1"
+elif [ "$1" == "custom_feature" ] && [ "$2" == 2 ] ; then
+    echo "Custom Feature 2"
 fi

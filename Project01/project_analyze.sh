a5a3e81b14c22d4dcf84867c9a7c6a0915c95fb3
salehh6 <salehh6@mcmaster.ca>
2020-02-28 23:57:08 +0000

    Done implementing features and custom features for Part 2.

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -11,13 +11,13 @@ elif [ "$1" == "feature" ] && [ "$2" == 1 ] ; then
 
 # Feature 2
 elif [ "$1" == "feature" ] && [ "$2" == 2 ] ; then
-    if [[ -e ~/private/CS1XA3/Project01/fixme.log ]] ; then
-        echo -n > ~/private/CS1XA3/Project01/fixme.log
+    if [[ -e  Project01/fixme.log ]] ; then
+        echo -n >  Project01/fixme.log
     fi
     grep -wRl "#FIXME" ~/private/CS1XA3 | while read -r file; do
           lastline=`tail -1 "$file"` 2> /dev/null
           if [[ "$lastline" == *"#FIXME"*  ]] ; then
-             echo "$file" >> ~/private/CS1XA3/Project01/fixme.log
+             echo "$file" >>  Project01/fixme.log
           fi
     done
     echo "File fixme.log was successfully created in Project01 directory."
@@ -47,22 +47,22 @@ elif [ "$1" == "feature" ] && [ "$2" == 4 ] ; then
 # Feature 5
 elif [ "$1" == "feature" ] && [ "$2" == 5 ] ; then
     read -p "Enter a file extention (i.e. sh, txt, py): " ext
-    grep -Rl --include=\*.${ext} "" ~/private/CS1XA3/ | wc -l
+    grep -Rl --include=\*.${ext} ""   | wc -l
 
 # Feature 6
 elif [ "$1" == "feature" ] && [ "$2" == 6 ] ; then
     read -p "Enter a TAG (i.e Any single word): " tag
     if [[ -n $tag ]] ; then 
-        if [[ -e ~/private/CS1XA3/Project01/${tag}.log ]] ; then
-            echo -n > ~/private/CS1XA3/Project01/${tag}.log
+        if [[ -e  Project01/${tag}.log ]] ; then
+            echo -n >  Project01/${tag}.log
         else
-            touch ~/private/CS1XA3/Project01/${tag}.log
+            touch  Project01/${tag}.log
         fi
         
         printed="false"
-        grep -Rh --include=\*.py '^#' ~/private/CS1XA3/ | while read -r line; do
+        grep -Rh --include=\*.py '^#'   | while read -r line; do
             if [[ "$line" == *"$tag"*  ]] ; then
-                echo "$line" >> ~/private/CS1XA3/Project01/${tag}.log
+                echo "$line" >>  Project01/${tag}.log
                 if [ $printed != "true" ] ; then
                     echo "File ${tag}.log was successfully created in the Project01 directory."
                     printed="true"
@@ -70,33 +70,33 @@ elif [ "$1" == "feature" ] && [ "$2" == 6 ] ; then
             fi
         done
     fi
-    if [[ ! -s ~/private/CS1XA3/Project01/${tag}.log ]] ; then
-        rm ~/private/CS1XA3/Project01/${tag}.log
+    if [[ ! -s  Project01/${tag}.log ]] ; then
+        rm  Project01/${tag}.log
     fi
 
 # Feature 7
 elif [ "$1" == "feature" ] && [ "$2" == 7 ] ; then
-    files=`grep -Rl --include=\*.sh "" ~/private/CS1XA3/`
+    files=`grep -Rl --include=\*.sh ""  `
     read -p "Type 'Change' if you wanna change permissions of the script files or 'Restore' if you want to restore their old permissions (without the quotes): " switch
     # Change
     if [[ "$switch" == "Change" ]] ; then
-        if [[ ! -e ~/private/CS1XA3/Project01/permissions.log ]] ; then
-            echo -n > ~/private/CS1XA3/Project01/permissions.log
+        if [[ ! -e  Project01/permissions.log ]] ; then
+            echo -n >  Project01/permissions.log
         fi
         while read -r file; do
             getfacl "$file" 2> /dev/null | while read -r line; do
                 if [[ "$line" == *"# file"* ]] ; then
                     current="$line"
-                    grep -q "$line" ~/private/CS1XA3/Project01/permissions.log
+                    grep -q "$line"  Project01/permissions.log
                     exit_status=$?
                     if [[ $exit_status == 0 ]] ; then
-                        lineNum=`grep -n "$line" ~/private/CS1XA3/Project01/permissions.log | head -n 1 | cut -d: -f1`
+                        lineNum=`grep -n "$line"  Project01/permissions.log | head -n 1 | cut -d: -f1`
                         continue
                     fi
-                    echo "$line" >> ~/private/CS1XA3/Project01/permissions.log
+                    echo "$line" >>  Project01/permissions.log
                 elif [[ "$line" == *"user::"* ]] ; then
                     if [[ $exit_status == 0 ]] ; then
-                        sed -i "$(( $lineNum+1 ))s|.*|${line}|" ~/private/CS1XA3/Project01/permissions.log
+                        sed -i "$(( $lineNum+1 ))s|.*|${line}|"  Project01/permissions.log
                         if [[ "$line" == *"w"* ]] ; then
                             chmod u+x "$file"
                         else 
@@ -104,7 +104,7 @@ elif [ "$1" == "feature" ] && [ "$2" == 7 ] ; then
                         fi
                         continue
                     fi
-                    echo "$line" >> ~/private/CS1XA3/Project01/permissions.log
+                    echo "$line" >>  Project01/permissions.log
                     if [[ "$line" == *"w"* ]] ; then
                         chmod u+x "$file"
                     else 
@@ -112,7 +112,7 @@ elif [ "$1" == "feature" ] && [ "$2" == 7 ] ; then
                     fi
                 elif [[ "$line" == *"group::"* ]] ; then
                     if [[ $exit_status == 0 ]] ; then
-                        sed -i "$(( $lineNum+2 ))s|.*|${line}|" ~/private/CS1XA3/Project01/permissions.log
+                        sed -i "$(( $lineNum+2 ))s|.*|${line}|"  Project01/permissions.log
                         if [[ "$line" == *"w"* ]] ; then
                             chmod g+x "$file"
                         else 
@@ -120,7 +120,7 @@ elif [ "$1" == "feature" ] && [ "$2" == 7 ] ; then
                         fi
                         continue
                     fi
-                    echo "$line" >> ~/private/CS1XA3/Project01/permissions.log
+                    echo "$line" >>  Project01/permissions.log
                     if [[ "$line" == *"w"* ]] ; then
                         chmod g+x "$file"
                     else
@@ -128,7 +128,7 @@ elif [ "$1" == "feature" ] && [ "$2" == 7 ] ; then
                     fi
                 elif [[ "$line" == *"other::"* ]] ; then
                     if [[ $exit_status == 0 ]] ; then
-                        sed -i "$(( $lineNum+3 ))s|.*|${line}|" ~/private/CS1XA3/Project01/permissions.log
+                        sed -i "$(( $lineNum+3 ))s|.*|${line}|"  Project01/permissions.log
                         if [[ "$line" == *"w"* ]] ; then
                             chmod o+x "$file"
                         else 
@@ -136,7 +136,7 @@ elif [ "$1" == "feature" ] && [ "$2" == 7 ] ; then
                         fi
                         continue
                     fi
-                    echo "$line" >> ~/private/CS1XA3/Project01/permissions.log
+                    echo "$line" >>  Project01/permissions.log
                     if [[ "$line" == *"w"* ]] ; then
                         chmod o+x "$file"
                     else
@@ -148,7 +148,7 @@ elif [ "$1" == "feature" ] && [ "$2" == 7 ] ; then
         echo "Permissions Changed."
     # Restore
     elif [[ "$switch" == "Restore" ]] ; then
-        if [[ -e ~/private/CS1XA3/Project01/permissions.log ]] ; then
+        if [[ -e  Project01/permissions.log ]] ; then
             while read -r rline; do
                 if [[ "$rline" == *"# file"* ]] ; then
                     filename="/${rline:8:${#rline}}"
@@ -159,7 +159,7 @@ elif [ "$1" == "feature" ] && [ "$2" == 7 ] ; then
                 elif [[ "$rline" == *"other::"* ]] ; then
                     chmod "o=${rline:7:${#rline}}" "$filename"
                 fi
-            done <<< `cat ~/private/CS1XA3/Project01/permissions.log`
+            done <<< `cat  Project01/permissions.log`
             echo "Permissions Restored."
         else
             echo "File permissions.log doesn't exist in Project01 directory."
@@ -179,12 +179,12 @@ elif [ "$1" == "custom_feature" ] && [ "$2" == 1 ] ; then
         wget -O file.txt "$url" 2>&1 | grep -i "error" > error.log
     done
     read -p "Enter the word that you wanna search in the file: " word
-    if [[ -e ~/private/CS1XA3/Project01/${word}.log ]] ; then
-        echo -n > ~/private/CS1XA3/Project01/${word}.log
+    if [[ -e  Project01/${word}.log ]] ; then
+        echo -n >  Project01/${word}.log
     fi
     while read -r line; do
         if [[ "$line" == *"$word"* ]] ; then
-            echo "$line" >> ~/private/CS1XA3/Project01/${word}.log
+            echo "$line" >>  Project01/${word}.log
         fi
     done <<< `cat file.txt`
     rm error.log
@@ -196,34 +196,34 @@ elif [ "$1" == "custom_feature" ] && [ "$2" == 2 ] ; then
     read -p "Type Add, Remove, Show or Save to add a TODO, remove a TODO, show the TODO list or save the TODO list into TODO.txt: " option
     # Add
     if [[ "$option" == "Add" ]] ; then
-        if [[ ! -e ~/private/CS1XA3/Project01/TODO.log ]] ; then
-            echo -n > ~/private/CS1XA3/Project01/TODO.log
+        if [[ ! -e  Project01/TODO.log ]] ; then
+            echo -n >  Project01/TODO.log
         fi 
         read -p "Enter your TODO's message here: " message
         hash=`echo "$message" | md5sum`
-        echo "${hash:0:32}"
-        echo "${hash} $message" >> ~/private/CS1XA3/Project01/TODO.log
-        #cat ~/private/CS1XA3/Project01/TODO.log
+        echo "${hash} $message" >>  Project01/TODO.log
+        echo "TODO's Hash number = ${hash:0:32}"
     # Remove
     elif [[ "$option" == "Remove" ]] ; then
-        read -p "Enter your TODO's number here: " number
-        sed -i -e "/${number}/d" ~/private/CS1XA3/Project01/TODO.log
+        read -p "Enter your TODO's Hash number here: " number
+        sed -i -e "/${number}/d"  Project01/TODO.log
+        echo "TODO with Hash number ${number} was removed."
     # Show
     elif [[ "$option" == "Show" ]] ; then
-        cat ~/private/CS1XA3/Project01/TODO.log
+        cat  Project01/TODO.log
     # Save
     elif [[ "$option" == "Save" ]] ; then
         counter=1
         while read -r line; do
-            echo "${counter} ${line:34:${#line}}" >> ~/private/CS1XA3/Project01/TODO.txt
+            echo "${counter} ${line:34:${#line}}" >>  Project01/TODO.txt
             counter=$(( counter+1 ))
-        done <<< `cat ~/private/CS1XA3/Project01/TODO.log`
-        cat ~/private/CS1XA3/Project01/TODO.txt
+        done <<< `cat  Project01/TODO.log`
+        echo "File TODO.txt was successfully created in the Project01 directory."
     else 
         echo "Usage: "
-        echo "Add    -> to Add a TODO to the list."
-        echo "Remove -> to Remove a TODO from the list."
-        echo "Save   -> to Save all the TODO's into a file."
+        echo "Add    -> to Add a TODO to the list. Prompts for TODO's message. "
+        echo "Remove -> to Remove a TODO from the list. Prompts for TODO's Hash number. "
+        echo "Show -> Shows all the current TODOs and their Hash numbers."
+        echo "Save   -> to Save all the TODO's into TODO.txt in the Project01 directory."
     fi
-    #echo "Custom Feature 2"
 fi

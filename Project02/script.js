28eef93742ea9b8263b7d1babf0a59db6d05e465
HeshSs <hishmat.salehi2015@gmail.com>
2020-03-15 12:58:31 -0400

    Added dark mode feature

--- Project02/script.js
+++ Project02/script.js
@@ -1,16 +1,35 @@
+//import {MDCRipple} from '@material/ripple';
+
+//const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+var counter = 0;
+
 function getRandomColor() {
-	  var letters = '0123456789ABCDEF';
-	  var color = '#';
-	  for (var i = 0; i < 6; i++) {
-		      color += letters[Math.floor(Math.random() * 16)];
-		    }
-	  return color;
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
 }
 
+function darkMode() {
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+
+	if (counter % 2 == 0) {
+	 	$(".resume-wrapper").css("background-color", "#212121");
+		$(".resume-wrapper").css("color", "white");
+	} else {
+		$(".resume-wrapper").css("background-color", "white");
+		$(".resume-wrapper").css("color", "black");
+	}
+	counter++;
+
+
+}
 
 $(document).ready(function(){
 	$(".bar").mousemove(function(){
-		  $(this).css("background-color", getRandomColor());
+		$(this).css("background-color", getRandomColor());
 	});
 });
-

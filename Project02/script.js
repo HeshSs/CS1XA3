39cb44fa1e52b65d7b2af9ae06d4ebc3cffe80f3
HeshSs <hishmat.salehi2015@gmail.com>
2020-03-15 00:20:05 -0400

    Added my first Javascript feature: bar random colorization.

---
+++ Project02/script.js
@@ -0,0 +1,16 @@
+function getRandomColor() {
+	  var letters = '0123456789ABCDEF';
+	  var color = '#';
+	  for (var i = 0; i < 6; i++) {
+		      color += letters[Math.floor(Math.random() * 16)];
+		    }
+	  return color;
+}
+
+
+$(document).ready(function(){
+	$(".bar").mousemove(function(){
+		  $(this).css("background-color", getRandomColor());
+	});
+});
+
